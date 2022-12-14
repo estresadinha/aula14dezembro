f065975cd6fbbae0c97c80429d5172ea64a1013b
estresadinha <120022356+estresadinha@users.noreply.github.com>
2022-12-14 11:58:26 -0300

    Add files via upload

---
+++ 14DEZEMBROAULA/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace _14DEZEMBROAULA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botaoLimpar = new System.Windows.Forms.Button();
+            this.senha = new System.Windows.Forms.TextBox();
+            this.label1Mensagem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // botaoLimpar
+            // 
+            this.botaoLimpar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.botaoLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoLimpar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.botaoLimpar.Location = new System.Drawing.Point(351, 62);
+            this.botaoLimpar.Name = "botaoLimpar";
+            this.botaoLimpar.Size = new System.Drawing.Size(75, 26);
+            this.botaoLimpar.TabIndex = 0;
+            this.botaoLimpar.Text = "limpar";
+            this.botaoLimpar.UseVisualStyleBackColor = false;
+            this.botaoLimpar.Click += new System.EventHandler(this.botaoLimpar_Click);
+            // 
+            // senha
+            // 
+            this.senha.Location = new System.Drawing.Point(55, 62);
+            this.senha.Multiline = true;
+            this.senha.Name = "senha";
+            this.senha.Size = new System.Drawing.Size(274, 23);
+            this.senha.TabIndex = 1;
+            this.senha.KeyDown += new System.Windows.Forms.KeyEventHandler(this.VerificaCaracter);
+            // 
+            // label1Mensagem
+            // 
+            this.label1Mensagem.AutoSize = true;
+            this.label1Mensagem.Location = new System.Drawing.Point(52, 140);
+            this.label1Mensagem.Name = "label1Mensagem";
+            this.label1Mensagem.Size = new System.Drawing.Size(35, 13);
+            this.label1Mensagem.TabIndex = 2;
+            this.label1Mensagem.Text = "label1";
+            this.label1Mensagem.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 201);
+            this.Controls.Add(this.label1Mensagem);
+            this.Controls.Add(this.senha);
+            this.Controls.Add(this.botaoLimpar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botaoLimpar;
+        private System.Windows.Forms.TextBox senha;
+        private System.Windows.Forms.Label label1Mensagem;
+    }
+}
+
---
+++ 14DEZEMBROAULA/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14DEZEMBROAULA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class ChecaForcaSenha
+        {
+
+            public enum ForcaDaSenha
+            {
+                Inaceitavel,
+                Fraca,
+                Aceitavel,
+                Forte,
+                Segura
+            }
+
+            public int geraPontosSenha(string senha)
+            {
+                if (senha == null) return 0;
+                int pontosPorTamanho = GetPontoPorTamanho(senha);
+                int pontosPorMinusculas = GetPontoPorMinusculas(senha);
+                int pontosPorMaiusculas = GetPontoPorMaiusculas(senha);
+                int pontosPorDigitos = GetPontoPorDigitos(senha);
+                int pontosPorSimbolos = GetPontoPorSimbolos(senha);
+                int pontosPorRepeticao = GetPontoPorRepeticao(senha);
+                return pontosPorTamanho + pontosPorMinusculas +
+                    pontosPorMaiusculas + pontosPorDigitos +
+                    pontosPorSimbolos - pontosPorRepeticao;
+            }
+
+            private int GetPontoPorTamanho(string senha)
+            {
+                return Math.Min(10, senha.Length) * 7;
+            }
+
+            private int GetPontoPorMinusculas(string senha)
+            {
+                int rawplacar = senha.Length - Regex.Replace(senha, "[a-z]", "").Length;
+                return Math.Min(2, rawplacar) * 5;
+            }
+
+            private int GetPontoPorMaiusculas(string senha)
+            {
+                int rawplacar = senha.Length - Regex.Replace(senha, "[A-Z]", "").Length;
+                return Math.Min(2, rawplacar) * 5;
+            }
+
+            private int GetPontoPorDigitos(string senha)
+            {
+                int rawplacar = senha.Length - Regex.Replace(senha, "[0-9]", "").Length;
+                return Math.Min(2, rawplacar) * 6;
+            }
+
+            private int GetPontoPorSimbolos(string senha)
+            {
+                int rawplacar = Regex.Replace(senha, "[a-zA-Z0-9]", "").Length;
+                return Math.Min(2, rawplacar) * 5;
+            }
+
+            private int GetPontoPorRepeticao(string senha)
+            {
+                System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"(\w)*.*\1");
+                bool repete = regex.IsMatch(senha);
+                if (repete)
+                {
+                    return 30;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            public ForcaDaSenha GetForcaDaSenha(string senha)
+            {
+                int placar = geraPontosSenha(senha);
+
+                if (placar < 50)// se if for <50 inaceitavel
+                    return ForcaDaSenha.Inaceitavel;
+                else if (placar < 60) // se placar for < 60 fraca
+                    return ForcaDaSenha.Fraca;
+                else if (placar < 80) // se placar for <80 aceitavel
+                    return ForcaDaSenha.Aceitavel;
+                else if (placar < 100)
+                    return ForcaDaSenha.Forte; // se placar for < 100 forte
+                else
+                    return ForcaDaSenha.Segura;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botaoLimpar_Click(object sender, EventArgs e)
+        {
+            senha.Text = "";
+            label1Mensagem.Text = "";
+        }
+
+        private void VerificaCaracter(object sender, KeyEventArgs e) // O Verificacaracteresta no textbox para verificar a sua senha
+        {
+            ChecaForcaSenha Verifica = new ChecaForcaSenha();// essa linha acessa a classe para verifica a checaforcasenha
+            ChecaForcaSenha.ForcaDaSenha forca;// essa linha e pra criar objto que vai guarda os valores 
+            forca = Verifica.GetForcaDaSenha(senha.Text); // essa linha consegue verificar o metedo classe somando pra ver se a senha esta forte esta no textbox na propriedade
+            label1Mensagem.Text = forca.ToString();// essa linha é o nome da label e forca guarda as variaveis, mostra como vai aparecer
+        }
+    }
+}
